27c7370fac226a59fb578d4da07942b3f2243f58
kulsoomMujtaba <kulsoom@codesorbit.com>
2020-09-20 15:23:16 +0500

    app_id updated

--- js/script.js
+++ js/script.js
@@ -1,47 +1,43 @@
 $(document).ready(function(){
 
+  let app_id="510";
+  //on Click Listeners
     $("#searchButton").click(function(){
+      //extract search text
       var artistInput = $("#searchText").val().trim();
-      getArtists("maroon5");
-      // getEvents("maroon5");
-      // alert(" input: "+artistInput);
-      // getArtists(artistInput);
-      // getEvents(artistInput);
+      getArtists(artistInput, app_id);
+      $("#searchText").val("");
     });
 
-    //on- set on click for artist card that will be generated dynamically 
-    $("#profile").on("click",".artistCard",function(){
-      var artist = $("#artistName").html();
-        changeUI();
-        getEvents(artist);
+    //on- set on click for artist card that will be generated dynamically in the future
+    $(".artist").on("click",".artistCard",function(){
+      var artist = $(".artistName").html();
+        updateUIEventPage();
+        getEvents(artist,app_id);
+    });
+
+    //Move back from event search results
+    $("#back").click(function(){
+      updateUISearchPage();
     });
   
   });
 
-  function getArtists(artistName) {
-    //  alert(" in func : helooo"+ artistName);
-    let queryArtist = 'https://rest.bandsintown.com/artists/' + artistName + '?app_id=codingbootcamp';
-          // alert(" in func : "+ queryArtist);
-
+  function getArtists(artistName,app_id) {
+    let queryArtist = 'https://rest.bandsintown.com/artists/' + artistName + '?app_id='+app_id;
     // Run API to get the artist
     $.ajax({
       url: queryArtist,
       method: "GET",
       success:function(response) {
-        // if(response){       
-        //   alert( (response + '').length );
-        // }
         $(".artist").empty();
-        console.log("image url: ");
-        // $("#artistSearchResults").text().replace("-1", "1");
-        // $.each(response, function(data) {
-            // console.log("image url: "+response[i].image_url);
+        // $.each(response, function(i,data) {
               $("#numberOfArtist").html(response.length);
               card=$("<div  class=\"artistCard card column col-xl-3 col-lg-4 col-md-5 col-sm-8  col-8\">");
               cardBody=$(" <div class=\"card-body text-center\">");
-              cardBody.append( "<img src="+response.image_url+ " class=\"rounded-circle mr-3\" height=\"70px\" width=\"70px\" alt=\"Profile Picture\"></img>");
-              cardBody.append("<h5 id=\"artistName\">"+response.name+"</h5>");
-              cardBody.append("<p>"+response.facebook_page_url+"</p>");
+              cardBody.append( "<img src="+response.image_url+ " class=\"rounded-circle mr-3 profileImage \" alt=\"Profile Picture\"></img>");
+              cardBody.append("<h5 class=\"artistName\">"+response.name+"</h5>");
+              cardBody.append("<p class=\"url\">"+response.facebook_page_url+"</p>");
               card.append(cardBody);
               
               $(".artist").append(card);
@@ -51,32 +47,21 @@ $(document).ready(function(){
     });
   }
 
-  function getEvents(artistName){
-    let queryEvent  = 'https://rest.bandsintown.com/artists/' + artistName + '/events?app_id=codingbootcamp';
+  function getEvents(artistName,app_id){
+    let queryEvent  = 'https://rest.bandsintown.com/artists/' + artistName + '/events?app_id='+app_id;
 
       // run API to get the events
       $.ajax({
         url: queryEvent,
         method: "GET",
         error:function(xhr, textStatus, errorThrown){
-          $(".js-no-artist").show();
-          $(".js-artist-info").hide();
+          // $(".js-no-artist").show();
+          // $(".js-artist-info").hide();
           },
         success:function(response) {
-
-          $(".js-no-artist").hide();
-          $(".js-artist-info").show();
-    
-          if (!response.length) {
-            $(".js-no-events").show();
-            $(".js-event-table").hide();
-          } else {
-            $(".js-event-table").show();
-            $(".js-no-events").hide();
             $(".event").empty();
             // Loop through results
-            $.each(response, function(i, data) {
-              if (i < 10) {
+            $.each(response, function(i,data) {
 
                 card=$("<div class=\"card column col-xl-3 col-lg-4 col-md-5 col-sm-8  col-11\">");
                   table=$("<table class=\"table table-borderless\">");
@@ -86,7 +71,6 @@ $(document).ready(function(){
                   table.append(title);
                   // County and City 
                   heading = $("<tr>");
-                  // event.append("<td class='td'>" + moment(data.datetime).format('MM/DD/YY') + "</td>" );
                   heading.append("<th class='th'>Country</th>" );
                   heading.append("<th class='th'>City</th>" );
 
@@ -111,13 +95,20 @@ $(document).ready(function(){
                   table.append(row);
                 card.append(table);
                 $(".event").append(card);
-              }
             });
-          }
         }
       });
      
     }
-    function changeUI(){
+    //Event page UI
+    function updateUIEventPage(){
       $("#searchBar").hide();
+      $("#back").show();
+      $("#eventPage").show();
+    }
+    //Search Page UI
+    function updateUISearchPage(){
+      $("#searchBar").show();
+      $("#back").hide();
+      $("#eventPage").hide();
     }
